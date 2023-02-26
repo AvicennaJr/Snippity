e30e1cf5ea94599a8152e6b1027b8d44a196ae71
AvicennaJr <justfuad@protonmail.com>
2023-02-26 18:49:31 +0300

    Add main script

---
+++ cmd/web/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on: %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
