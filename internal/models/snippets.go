c88cb6653f8529ec1584fb7f66942bd529dea321
AvicennaJr <justfuad@protonmail.com>
2023-02-26 23:27:28 +0300

    Add snippet wrapper to wrap connection pool

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	"time"
@@ -12,6 +13,10 @@ type Snippet struct {
 	Content string
 	Create  time.Time
 	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
 }
 
 func main() {
