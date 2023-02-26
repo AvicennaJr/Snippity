7a2f0cff9eac3516ce0114a1fa337cfcec55b7dc
AvicennaJr <justfuad@protonmail.com>
2023-02-26 23:39:49 +0300

    Add snippet field to application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/AvicennaJr/Snippity/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
