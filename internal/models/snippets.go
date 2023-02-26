9f1f03a48381e82619b8083c92b4e75e0bb6b9b0
AvicennaJr <justfuad@protonmail.com>
2023-02-27 00:00:59 +0300

    Add handling insert query

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -20,7 +20,20 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
-	return 0, nil
+	stmt := "insert into snippets (title, content, created, expires) values (?, ?, utc_timestamp(), date_add(utc_timestamp(), interval ? day))"
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
