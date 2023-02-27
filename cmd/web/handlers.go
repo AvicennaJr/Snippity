d9566fb9b005cb4ff96d66d1b4fb958f598d6d64
AvicennaJr <justfuad@protonmail.com>
2023-02-27 23:51:33 +0300

    Add create page to create handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -54,7 +54,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
