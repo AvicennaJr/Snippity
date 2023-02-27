2c79b4738003d10da9de0d7833aed293ec2d49fa
AvicennaJr <justfuad@protonmail.com>
2023-02-27 18:12:57 +0300

    Add snippets field to template struct

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/AvicennaJr/Snippity/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
