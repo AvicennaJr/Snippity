699b149f241424338d432b7db76f1f68e0b352ef
AvicennaJr <justfuad@protonmail.com>
2023-02-27 21:55:32 +0300

    Fixed type font -> fonts

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' font.googleapis.com; font-src font.gstatic.com")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
