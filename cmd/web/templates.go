98cde05727221cf026d57f63bd117ac7520325d7
AvicennaJr <justfuad@protonmail.com>
2023-02-27 18:33:22 +0300

    Add ability to cache templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,42 @@
 package main
 
-import "github.com/AvicennaJr/Snippity/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/AvicennaJr/Snippity/internal/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*tmpl")
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
