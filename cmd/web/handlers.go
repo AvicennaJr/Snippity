40cf7f81222c33184f21922c02c8e022a20bdeb7
AvicennaJr <justfuad@protonmail.com>
2023-02-28 01:12:57 +0300

    Initialize values on snippetCreate

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -65,6 +65,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
