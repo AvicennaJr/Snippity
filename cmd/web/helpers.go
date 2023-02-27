d274a28593e8a09869edd48d800aa9f89a534948
AvicennaJr <justfuad@protonmail.com>
2023-02-27 18:42:02 +0300

    Add a helper function to cache and display pages in memory

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -20,3 +20,19 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
+
+func (app *application) render(w http.ResponseWriter, status int, page string, data templateData) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("The template does not exist: %s", page)
+		app.serveError(w, err)
+		return
+	}
+
+	w.WriteHeader(status)
+
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serveError(w, err)
+	}
+}
