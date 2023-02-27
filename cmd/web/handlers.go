0e4fdadbf052e00393ab7d34fe970c62a62587e3
AvicennaJr <justfuad@protonmail.com>
2023-02-28 00:00:32 +0300

    Add ability to create snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -61,13 +61,24 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
-	title := "Something"
-	content := "Long ago in the past, some turtles were fighting for BLM."
-	expires := 7
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("")
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serveError(w, err)
+		return
 	}
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
