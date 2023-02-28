c97f4066a46818ee8bf866e6451166b707a91896
AvicennaJr <justfuad@protonmail.com>
2023-02-28 17:20:45 +0300

    use request ctx instead of session data to check authentication

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -78,5 +78,10 @@ func (app *application) decodePostForm(r *http.Request, destination any) error {
 }
 
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+
+	return isAuthenticated
 }
